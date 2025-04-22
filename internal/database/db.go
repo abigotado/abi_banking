eec80902e6bc5b63e5e2d96fb9cacfc88ea47c44
Nikita Kovalenko <nik.koval.89@gmail.com>
2025-04-22 17:21:54 -0300

    feat: add docker-compose and environment configuration

--- internal/database/db.go
+++ internal/database/db.go
@@ -3,62 +3,49 @@ package database
 import (
 	"database/sql"
 	"fmt"
-	"log"
 	"os"
-	"time"
 
 	_ "github.com/lib/pq"
+	"github.com/sirupsen/logrus"
 )
 
 var DB *sql.DB
 
-type Config struct {
-	Host     string
-	Port     string
-	User     string
-	Password string
-	DBName   string
-}
+func InitDB(logger *logrus.Logger) error {
+	// Get database configuration from environment variables
+	dbHost := getEnvOrDefault("DB_HOST", "localhost")
+	dbPort := getEnvOrDefault("DB_PORT", "5438")
+	dbUser := getEnvOrDefault("DB_USER", "postgres")
+	dbPassword := getEnvOrDefault("DB_PASSWORD", "postgres")
+	dbName := getEnvOrDefault("DB_NAME", "abi_banking")
+	dbSSLMode := getEnvOrDefault("DB_SSL_MODE", "disable")
 
-func NewConfig() *Config {
-	return &Config{
-		Host:     getEnv("DB_HOST", "localhost"),
-		Port:     getEnv("DB_PORT", "5432"),
-		User:     getEnv("DB_USER", "postgres"),
-		Password: getEnv("DB_PASSWORD", ""),
-		DBName:   getEnv("DB_NAME", "banking"),
+	// Construct connection string
+	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		dbHost, dbPort, dbUser, dbPassword, dbName, dbSSLMode)
+
+	// Open database connection
+	var err error
+	DB, err = sql.Open("postgres", connStr)
+	if err != nil {
+		return fmt.Errorf("failed to open database connection: %w", err)
 	}
+
+	// Test the connection
+	if err = DB.Ping(); err != nil {
+		return fmt.Errorf("failed to ping database: %w", err)
+	}
+
+	logger.Info("Successfully connected to database")
+	return nil
 }
 
-func getEnv(key, defaultValue string) string {
+func getEnvOrDefault(key, defaultValue string) string {
 	value := os.Getenv(key)
 	if value == "" {
 		return defaultValue
 	}
 	return value
-}
-
-func InitDB() error {
-	config := NewConfig()
-	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-		config.Host, config.Port, config.User, config.Password, config.DBName)
-
-	var err error
-	DB, err = sql.Open("postgres", connStr)
-	if err != nil {
-		return fmt.Errorf("error opening database: %v", err)
-	}
-
-	DB.SetMaxOpenConns(25)
-	DB.SetMaxIdleConns(25)
-	DB.SetConnMaxLifetime(5 * time.Minute)
-
-	if err = DB.Ping(); err != nil {
-		return fmt.Errorf("error connecting to the database: %v", err)
-	}
-
-	log.Println("Successfully connected to database")
-	return nil
 }
 
 func CloseDB() error {
